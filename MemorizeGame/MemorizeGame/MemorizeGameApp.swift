4b23c9eb4a9654c74991edc829fb62c8944a66de
Narasimha Baliga <bnbaliga@Narasimhas-iMac.home>
2023-11-15 11:53:28 -0600

    first time checking in

---
+++ MemorizeGame/MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Narasimha Baliga on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
